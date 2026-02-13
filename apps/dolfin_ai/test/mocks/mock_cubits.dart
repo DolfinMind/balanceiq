d9c8e0c122ef6a2feeda126ff1518351bf871626
Sifat Ullah Chowdhury <sif.sifat24@gmail.com>
2026-02-14 00:15:15 +0600

    Fix tests in app dolfin ai

--- apps/dolfin_ai/test/mocks/mock_cubits.dart
+++ apps/dolfin_ai/test/mocks/mock_cubits.dart
@@ -3,9 +3,9 @@ import 'package:dolfin_ui_kit/theme/theme_cubit.dart';
 import 'package:dolfin_core/tour/product_tour_cubit.dart';
 import 'package:feature_auth/presentation/cubit/interactive_onboarding/interactive_onboarding_cubit.dart';
 import 'package:feature_auth/presentation/cubit/login/login_cubit.dart';
-import 'package:feature_auth/presentation/cubit/password/password_cubit.dart';
+
 import 'package:feature_auth/presentation/cubit/session/session_cubit.dart';
-import 'package:feature_auth/presentation/cubit/signup/signup_cubit.dart';
+
 import 'package:feature_chat/presentation/cubit/chat_cubit.dart';
 import 'package:balance_iq/features/home/presentation/cubit/dashboard_cubit.dart';
 import 'package:balance_iq/features/home/presentation/cubit/transaction_filter_cubit.dart';
@@ -39,11 +39,6 @@ class MockTransactionFilterCubit extends MockCubit<TransactionFilterState>
     implements TransactionFilterCubit {}
 
 class MockChatCubit extends MockCubit<ChatState> implements ChatCubit {}
-
-class MockPasswordCubit extends MockCubit<PasswordState>
-    implements PasswordCubit {}
-
-class MockSignupCubit extends MockCubit<SignupState> implements SignupCubit {}
 
 class MockInteractiveOnboardingCubit
     extends MockCubit<InteractiveOnboardingState>
--- apps/dolfin_ai/test/mocks/mock_usecases.dart
+++ apps/dolfin_ai/test/mocks/mock_usecases.dart
@@ -1,8 +1,5 @@
-import 'package:feature_auth/domain/usecases/login.dart';
 import 'package:feature_auth/domain/usecases/sign_in_with_google.dart';
-import 'package:feature_auth/domain/usecases/signup.dart';
-import 'package:feature_auth/domain/usecases/send_verification_email.dart';
-import 'package:feature_auth/domain/usecases/resend_verification_email.dart';
+
 import 'package:feature_auth/domain/usecases/get_current_user.dart';
 import 'package:feature_auth/domain/usecases/sign_out.dart';
 import 'package:feature_auth/domain/usecases/get_profile.dart';
@@ -14,16 +11,7 @@ import 'package:feature_chat/domain/usecases/submit_feedback.dart';
 import 'package:feature_chat/domain/usecases/update_message.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockLogin extends Mock implements Login {}
-
 class MockSignInWithGoogle extends Mock implements SignInWithGoogle {}
-
-class MockSignup extends Mock implements Signup {}
-
-class MockSendVerificationEmail extends Mock implements SendVerificationEmail {}
-
-class MockResendVerificationEmail extends Mock
-    implements ResendVerificationEmail {}
 
 class MockGetCurrentUser extends Mock implements GetCurrentUser {}
 
